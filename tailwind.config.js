91938b2cc52623b08ac94035d6a341ce955ea882
Ruslan321532 <ruslanburalkiev8@gmail.com>
2023-04-02 23:13:35 +0600

    next js test

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// module.exports = {
+//   content: [
+//     "./app/**/*.{js,ts,jsx,tsx}",
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+
+//     // Or if using `src` directory:
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
